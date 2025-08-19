57cf855b9e6e2988f9feff9ff8a9297292fe844a
Nathan <jamesnathan1324@gmail.com>
2025-08-19 11:44:31 -0500

    Initial commit with farm game code

---
+++ farm_idle.py
@@ -0,0 +1,46 @@
+gold = 20
+farm_inv = {
+    "crops": {
+        "pepper": 3,
+        "carrot": 0,
+        "pea": 0,
+        "cucumber": 0,
+        "eggplant": 0,
+        "radish": 0,
+        "onion": 0,
+        "hops": 0,
+        "potato": 0,
+        "tomato": 0,
+        "leek": 0,
+        "watermelon": 0,
+        "corn": 0,
+        "cabbage": 0,
+        "pine": 0,
+        "pumpkin": 0
+         }
+    }
+
+crops = {
+    "pepper": 0,
+    "carrot": 0,
+    "pea": 0,
+    "cucumber": 0,
+    "eggplant": 0,
+    "radish": 0,
+    "onion": 0,
+    "hops": 0,
+    "potato": 0,
+    "tomato": 0,
+    "leek": 0,
+    "watermelon": 0,
+    "corn": 0,
+    "cabbage": 0,
+    "pine": 0,
+    "pumpkin": 0
+         }
+
+
+
+
+
+    
